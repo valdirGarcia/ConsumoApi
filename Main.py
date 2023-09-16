ece0e746c14338438ecc51b4a1ffaa1831f40de8
valdir <valdirjc2002@gmail.com>
2023-09-16 11:09:44 -0300

    quebra de funcoes na main e transformar_json de publico para privado

--- Main.py
+++ Main.py
@@ -1,9 +1,41 @@
 from StarWars import StarWars
 
+def consultar_personagem():
+    id_personagem = input("Digite o ID do personagem desejado: ")
+    dados_personagem = sw_api.obter_personagem(id_personagem)
+    if dados_personagem:
+        print(f"\nInformações sobre o personagem {id_personagem}:")
+        print("Nome:", dados_personagem['name'])
+        print("Altura:", dados_personagem['height'])
+        print("Peso:", dados_personagem['mass'])
+    else:
+        print("\nPersonagem não encontrado ou falha na solicitação da API.")
+
+def consultar_filme():
+    id_filme = input("Digite o ID do filme desejado: ")
+    dados_filme = sw_api.obter_filme(id_filme)
+    if dados_filme:
+        print(f"\nInformações sobre o filme {id_filme}:")
+        print("Título:", dados_filme['title'])
+        print("Diretor:", dados_filme['director'])
+        print("Lançamento:", dados_filme['release_date'])
+    else:
+        print("\nFilme não encontrado ou falha na solicitação da API.")
+
+def consultar_planeta():
+    id_planeta = input("Digite o ID do planeta desejado: ")
+    dados_planeta = sw_api.obter_planeta(id_planeta)
+    if dados_planeta:
+        print(f"\nInformações sobre o planeta {id_planeta}:")
+        print("Nome:", dados_planeta['name'])
+        print("Clima:", dados_planeta['climate'])
+        print("Terreno:", dados_planeta['terrain'])
+    else:
+        print("\nPlaneta não encontrado ou falha na solicitação da API.")
+
 sw_api = StarWars()
 
 while True:
-    
     print("\nSelecione uma opção:")
     print("1 - Consultar dados de personagem")
     print("2 - Consultar dados de filme")
@@ -13,35 +45,11 @@ while True:
     opcao = input("Digite o número da opção desejada: ")
 
     if opcao == "1":
-        id_personagem = input("Digite o ID do personagem desejado: ")
-        dados_personagem = sw_api.obter_personagem(id_personagem)
-        if dados_personagem:
-            print(f"\nInformações sobre o personagem {id_personagem}:")
-            print("Nome:", dados_personagem['name'])
-            print("Altura:", dados_personagem['height'])
-            print("Peso:", dados_personagem['mass'])
-        else:
-            print("\nPersonagem não encontrado ou falha na solicitação da API.")
+        consultar_personagem()
     elif opcao == "2":
-        id_filme = input("Digite o ID do filme desejado: ")
-        dados_filme = sw_api.obter_filme(id_filme)
-        if dados_filme:
-            print(f"\nInformações sobre o filme {id_filme}:")
-            print("Título:", dados_filme['title'])
-            print("Diretor:", dados_filme['director'])
-            print("Lançamento:", dados_filme['release_date'])
-        else:
-            print("\nFilme não encontrado ou falha na solicitação da API.")
+        consultar_filme()
     elif opcao == "3":
-        id_planeta = input("Digite o ID do planeta desejado: ")
-        dados_planeta = sw_api.obter_planeta(id_planeta)
-        if dados_planeta:
-            print(f"\nInformações sobre o planeta {id_planeta}:")
-            print("Nome:", dados_planeta['name'])
-            print("Clima:", dados_planeta['climate'])
-            print("Terreno:", dados_planeta['terrain'])
-        else:
-            print("\nPlaneta não encontrado ou falha na solicitação da API.")
+        consultar_planeta()
     elif opcao == "4":
         print("\nEncerrando o programa.")
         break
--- StarWars.py
+++ StarWars.py
@@ -5,7 +5,7 @@ class StarWars:
     def __init__(self):
         self.base_url = "https://swapi.dev/api/"
 
-    def transformar_json(self, url):
+    def _transformar_json(self, url):
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
@@ -18,12 +18,12 @@ class StarWars:
 
     def obter_personagem(self, id_personagem):
         url = f"{self.base_url}people/{id_personagem}/"
-        return self.transformar_json(url)
+        return self._transformar_json(url)
 
     def obter_filme(self, id_filme):
         url = f"{self.base_url}films/{id_filme}/"
-        return self.transformar_json(url)
+        return self._transformar_json(url)
 
     def obter_planeta(self, id_planeta):
         url = f"{self.base_url}planets/{id_planeta}/"
-        return self.transformar_json(url)
+        return self._transformar_json(url)
